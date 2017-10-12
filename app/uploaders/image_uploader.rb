45173140c207049287dc230dc7b11986120a4a89
anjarupnik <anjarupnik@hotmail.com>
2017-10-12 10:47:17 +0200

    Add Cloudinary and update image uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,9 @@
 
 class ImageUploader < CarrierWave::Uploader::Base
 
+  include Cloudinary::CarrierWave
+
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
